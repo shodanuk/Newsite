930ad12e46e0e46d99ed0851d3362ddce20baee5
Terry Morgan <terry.morgan76@gmail.com>
2011-04-24 16:31:38 +0100

    - Moved virtual host declaration into main script and deleted default.txt - Set permissions on site root folder so the user can write to it. - Added OWNER variable for username to be made owner of new site root folder - Fixed missing .localhost in hosts entry

--- newsite.sh
+++ newsite.sh
@@ -2,13 +2,8 @@
 
 # We're storing each virtual host definition os it's own
 # conf file in the following directory:
- 
+
 VHOSTDIR="/etc/apache2/other"
-
-# The following file contains the virtual host template
-# that we're basing our new virtual host definition on
-
-DEFAULTCONF="default.txt"
 
 # Path to the sites folder where the new sites root folder
 # will live
@@ -23,38 +18,55 @@ HOSTSFILE="/etc/hosts"
 
 HOSTSIP="127.0.0.1"
 
-# host_exists function yoinked from 
+# Your username. The site root folder will need to be owned by you
+# user for you to be able to edit it.
+
+OWNER="Shodan"
+
+# host_exists function yoinked from
 # https://github.com/pgib/virtualhost.sh
 host_exists()
 {
-	if grep -q -e "^$HOSTSIP    $1$" $HOSTSFILE ; then
-		return 0
-	else
-		return 1
-	fi
+  if grep -q -e "^$HOSTSIP    $1$" $HOSTSFILE ; then
+    return 0
+  else
+    return 1
+  fi
 }
 
 # Create new site root folder
 
 if [ ! -d "$SITESDIR/$1" ]; then
   echo "Creating site folder..."
-  mkdir "$SITESDIR/$1"  
 
-  # Copy and amend default virtual host entry
+  SITEROOT="$SITESDIR/$1"
+  VHOST="$VHOSTDIR/$1.conf"
+  LOCALHOST="$1.localhost"
+
+  mkdir $SITEROOT
+  chown $OWNER $SITEROOT
+
+  # Set up n virtual host entry
 
   echo "Setting up vhost..."
-  touch $1.conf
-  cat $DEFAULTCONF | sed "s/xxxx/$(echo $1 | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g')/g" > $1.conf
-  mv $1.conf "$VHOSTDIR/$1.conf"
+
+  touch $VHOST
+  cat << __EOF >$VHOST
+# Created by newsite.sh
+<VirtualHost *:80>
+    DocumentRoot $SITEROOT
+    ServerName $LOCALHOST
+    ErrorLog "/private/var/log/apache2/$1.localhost-error_log"
+    CustomLog "/private/var/log/apache2/$1.localhost-access_log" common
+</VirtualHost>
+__EOF
 
   # Add an entry to the hosts file, if one doesn't already exist
 
-  HOST="$1.localhost"
-
   if ! host_exists $1 ; then
-    /bin/echo "Creating a hosts file entry for $HOST..."
-    /bin/echo -n "+ Adding $HOST to $HOSTSFILE... "
-    /bin/echo "$HOSTSIP    $1" >> $HOSTSFILE
+    /bin/echo "Creating a hosts file entry for $LOCALHOST..."
+    /bin/echo -n "+ Adding $LOCALHOST to $HOSTSFILE... "
+    /bin/echo "$HOSTSIP    $LOCALHOST" >> $HOSTSFILE
     /bin/echo "done"
   fi
 

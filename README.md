5e9fb7122ced1addb5284fb8f5c81615c80a0779
Terry Morgan <terry.morgan76@gmail.com>
2011-04-24 16:43:49 +0100

    - Updated readme and added more comments to code.
    - Added MIT licensing.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # newsite.sh
 
-Sets up a new site on Mac OSx Snow Leopard.  
+Sets up a new site on Mac OSx Snow Leopard.
 
 * Creates site root folder
 * Sets up a new Virtual Host
@@ -9,22 +9,37 @@ Sets up a new site on Mac OSx Snow Leopard.
 
 ## Usage
 
-Move (or symlink) newsite.sh and default.txt to /usr/bin (or somewhere appropriate)
-
-Then run:
-
+1. Update all config options at the top of newsite.sh to suit your system and requirements
+2. Move (or symlink) newsite.sh and default.txt to /usr/bin (or somewhere appropriate)
+3. Then run:
 ```
 sudo newsite.sh sitename
 ```
 
-That will create:
+newsite.sh will create:
 
 * ~/Sites/sitename folder
 * sitename.conf in /etc/apache2/other
 * 127.0.0.1 sitename.localhost in /etc/hosts
 
-## Assumptions
+## MIT Licence
 
-Your httpd.conf is configured to load all .conf files in /etc/apache2/other
+Copyright (c) 2011 Terry Morgan
 
- 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
--- newsite.sh
+++ newsite.sh
@@ -1,12 +1,47 @@
 #!/usr/bin/env bash
 
+#===================================================================================
+
+# newsite.sh by Terry Morgan <terry.morgan@marmaladeontoast.co.uk>
+#
+# A little script to set up new development sites on Mac OSX
+#
+# host_exists function and other bits yoinked from Patrick Gibson's virtualhost.sh:
+# https://github.com/pgib/virtualhost.sh
+#
+# MIT License
+#
+# Copyright (c) 2011 Terry Morgan
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+#===================================================================================
+
+# ---------------- Start Configuration options -----------------
+# Review all configuration options and amend to suit your system and requirements
+
 # We're storing each virtual host definition os it's own
 # conf file in the following directory:
 
 VHOSTDIR="/etc/apache2/other"
 
-# Path to the sites folder where the new sites root folder
-# will live
+# Path to the sites folder where the new sites root folder will live
 
 SITESDIR="$HOME/Sites"
 
@@ -18,13 +53,13 @@ HOSTSFILE="/etc/hosts"
 
 HOSTSIP="127.0.0.1"
 
-# Your username. The site root folder will need to be owned by you
+# Change this to your username. The site root folder will need to be owned by you
 # user for you to be able to edit it.
 
 OWNER="Shodan"
 
-# host_exists function yoinked from
-# https://github.com/pgib/virtualhost.sh
+# ---------------- END Configuration options -----------------
+
 host_exists()
 {
   if grep -q -e "^$HOSTSIP    $1$" $HOSTSFILE ; then
@@ -74,9 +109,9 @@ __EOF
 
   apachectl -k graceful
 
-  echo "All done :)"
+  echo "All done. You da man! :)"
 else
-  echo "Site folder called $1 already exists. Bailing out..."
+  echo "<sadface> Site folder called $1 already exists. Bailing out... </ sadface>"
 fi
 
 exit
